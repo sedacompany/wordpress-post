7b34499c82dd6fdf8d3ef5ceef7c5c79f935bbb9
sedacompany <seda.company.official@gmail.com>
2025-03-19 08:45:15 +0000

    Initial commit

---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'gDEE3a9Wz1eL0Vccvzx9VCx1iGV8bBy05iwqL4CP0VMmSrU4xrhAKvrmdBbGPsxh');
+define('SECURE_AUTH_KEY',  'XIQPJYhYa8v0ctxSdR8pc0Sgyuw86jpsJYxSfzwHVbi1W385IT0Pzr3cptTLseuS');
+define('LOGGED_IN_KEY',    'wo8tXtFhiiohuHabQi9atwiFUWzecai5oqtMoxUy96ujn6nHrTqgRIuvDYuHWx0h');
+define('NONCE_KEY',        'BB9JeY4VRCJey8q2erBKpBFS84hrmHz6FLac5WbVG5U0CoP6CXSa5VoiLxxpL3M4');
+define('AUTH_SALT',        'S1NUQJXT0smr2mR2AsayUbm38N6JUbmwf9rIHVyxXb1om8RjNMM41xswDDySpBLP');
+define('SECURE_AUTH_SALT', '8mj80aEYU9v6U6YjuIoXsFpUYV7EDyj4hpnP8gBM76Mtw8tzCDn68DGcCb4egzDz');
+define('LOGGED_IN_SALT',   'uu6jzQz5BtwSfBCLsIMfdarRHnPEn8nW85MX9t4y8NF4rz6Hp43UMsrNjF5VUqNX');
+define('NONCE_SALT',       'wABDg7T1uPJxnsGJydA8aAe3uie5IQvCRrMPQ48zYtfYJntQEwYEPTEtgcDsEN2J');
